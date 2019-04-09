4dab5b82b62023d214693491ce9a62b932140ade
sotirelisc <gtakriz@hotmail.com>
2019-04-09 22:02:58 +0200

    Hook up YouTube API & create components for videos.

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
+  state = {
+    videos: []
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar onSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }

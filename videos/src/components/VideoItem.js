bae082509dc03dff2a70dc2c0258c2e4ba7ef7b8
sotirelisc <gtakriz@hotmail.com>
2019-04-09 22:27:05 +0200

    Show video results & add basic CSS.

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './VideoItem.css';
 
-const VideoItem = props => {
+const VideoItem = ({ video }) => {
   return (
-    <div>
-
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}  
+      />
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
     </div>
   );
 };
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    console.log(video);
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+
   return (
-    <div>
-      {videos.length}
+    <div className="ui relaxed divided list">
+      {renderedList}
     </div>
   );
 };

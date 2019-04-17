f49cbba7d41e6c84e9724ba9ec61830ec69e5cc9
sotirelisc <gtakriz@hotmail.com>
2019-04-18 00:01:15 +0300

    Implement sign-in with Google account.

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -11,20 +11,44 @@ class GoogleAuth extends React.Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({
-          isSignedIn: this.auth.isSignedIn.get()
-        });
+        this.onAuthChange();
+        // Listen to authentication changes & update state
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({
+      isSignedIn: this.auth.isSignedIn.get()
+    });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>Loading...</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>Sign Out</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>Sign In</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
 

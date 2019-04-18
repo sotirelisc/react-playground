248cd7078510c716bf35ae3f5e37188379647175
sotirelisc <gtakriz@hotmail.com>
2019-04-18 14:27:53 +0300

    Add ReduxForm & config for create streams form handling.

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+  renderInput({ input, label, meta }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        <div>{meta.error}</div>
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field
+          name="title"
+          label="Enter Title"
+          component={this.renderInput}
+        />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">
+          Submit
+        </button>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+const validate = ({ title, description }) => {
+  const errors = {};
+
+  if (!title) {
+    errors.title = 'Please enter a title.'
+  }
+
+  if (!description) {
+    errors.description = 'Please enter a description.'
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate
+})(StreamCreate);

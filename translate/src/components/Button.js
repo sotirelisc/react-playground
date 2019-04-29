5f4b09ca7ab1c6bc6c420844e4f6e026450221d0
sotirelisc <gtakriz@hotmail.com>
2019-04-30 00:22:26 +0300

    Create basic context object.

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
     return <button className="ui button primary">Submit</button>;
   }
